68f5d7e1a494b59a76dad755778da807419a7658
Tyler Nodell <tyler.l.nodell@gmail.com>
2018-07-06 14:31:11 +0000

    automatically backed up by learn

--- simple_looping.rb
+++ simple_looping.rb
@@ -11,7 +11,7 @@ def loop_iterator(number_of_times)
   @phrase = "Welcome to Flatiron School's Web Development Course!"
   number = 0
   loop do
-    puts phrase
+    puts @phrase
     break if number == number_of_times
     number+=1
   end
